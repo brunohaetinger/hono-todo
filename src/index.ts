24dd28ed066cf2ffd3106810b685b1f03a878d0b
Bruno Haetinger <brunohaetinger@gmail.com>
2025-11-17 07:08:01 -0300

    Add POST /tasks

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import { Hono } from 'hono'
+import crypto from 'crypto';
 
 const app = new Hono()
+const tasks = [];
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('To-Do API')
+})
+
+app.post('/tasks', async(c) => {
+  const task = await c.req.json();
+  const newTask = {
+    id: crypto.randomUUID(),
+    title: task.title,
+    completed: false,
+  };
+  tasks.push(newTask);
+  return c.json(newTask, {status: 201});
 })
 
 export default app
