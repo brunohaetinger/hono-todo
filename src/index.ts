4471a73b43d54613256bf1eb4f043b8bda35cd8b
Bruno Haetinger <brunohaetinger@gmail.com>
2025-11-17 07:15:13 -0300

    Add GET /tasks by id

--- src/index.ts
+++ src/index.ts
@@ -19,4 +19,13 @@ app.post('/tasks', async(c) => {
   return c.json(newTask, {status: 201});
 })
 
+app.get('/tasks/:id', (c) => {
+  const id = c.req.param("id");
+  const task = tasks.find(t => t.id === id);
+  if(!task){
+    return c.json({message: 'Task not found'}, {status: 404})
+  }
+  return c.json(task);
+})
+
 export default app
