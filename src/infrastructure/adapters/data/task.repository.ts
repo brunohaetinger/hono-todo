c8b541bea2d87ac7c2c3fa29ff8323c72683300b
Bruno Haetinger <brunohaetinger@gmail.com>
2025-11-22 16:02:32 -0300

    Task repository

---
+++ src/infrastructure/adapters/data/task.repository.ts
@@ -0,0 +1,30 @@
+import { TaskRepository } from "../../../domain/tasks/repositories/task.repository";
+import { db } from '../../db/client';
+import { tasks } from '../../adapters/data/sqlite/schema'
+import { TaskId } from "../../../domain/tasks/entities/value-objects/task-id.vt";
+import { TaskTitle } from "../../../domain/tasks/entities/value-objects/task-title.vt";
+import { Task } from "../../../domain/tasks/entities/task.entity";
+
+export class TaskRepositoryImpl implements TaskRepository {
+  save(task: Task): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  delete(id: TaskId): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  update(task: Task): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  findById(id: TaskId): Promise<Task | null> {
+    throw new Error("Method not implemented.");
+  }
+
+  async findAll(): Promise<Task[]> {
+    const rows = await db.select().from(tasks);
+    return rows.map((r) => new Task(
+      new TaskId(r.id),
+      new TaskTitle(r.title),
+      r.completed
+    ))
+  }
+}
