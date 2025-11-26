e526059256c9bbb1de9edbed5b5016e2c2e778fb
Bruno Haetinger <brunohaetinger@gmail.com>
2025-11-25 23:14:39 -0300

    Wrong repository import

--- src/infrastructure/adapters/data/task.repository.ts
+++ src/infrastructure/adapters/data/task.repository.ts
@@ -1,19 +1,10 @@
-import { TaskRepository } from "../../../domain/tasks/repositories/task.repository";
-import { db } from '../../db';
-import { tasks } from '../../adapters/data/sqlite/schema'
-import { TaskId } from "../../../domain/tasks/entities/value-objects/task-id.vt";
-import { TaskTitle } from "../../../domain/tasks/entities/value-objects/task-title.vt";
 import { Task } from "../../../domain/tasks/entities/task.entity";
+import { TaskId } from "../../../domain/tasks/entities/value-objects/task-id.vt";
+import { TaskRepository } from "../../../domain/tasks/repositories/task.repository";
 
 export class TaskRepositoryImpl implements TaskRepository {
-  async save(task: Task): Promise<void> {
-    const row = await db.insert(tasks).values({
-      id: task.id.value,
-      title: task.title.value,
-      createdAt: new Date().getTime(),
-      // createdAt: task.createdAt.getTime(),
-    }).returning();
-    return;
+  save(task: Task): Promise<void> {
+    throw new Error("Method not implemented.");
   }
   delete(id: TaskId): Promise<void> {
     throw new Error("Method not implemented.");
@@ -26,11 +17,6 @@ export class TaskRepositoryImpl implements TaskRepository {
   }
 
   async findAll(): Promise<Task[]> {
-    const rows = await db.select().from(tasks);
-    return rows.map((r) => new Task(
-      new TaskId(r.id),
-      new TaskTitle(r.title),
-      Boolean(r.completed)
-    ))
+    return [];
   }
 }
